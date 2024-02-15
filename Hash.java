3410c574df6fe5bb2b59c71c1e321a1772bf44cc
mitalilP001 <mitalilpurohit@gmail.com>
2024-02-15 18:04:40 +0530

    code implementations of hash function

--- Hash.java
+++ Hash.java
@@ -24,3 +24,4 @@ public class Hash {
         return true; // Example implementation, you need to replace this
     }
 }
+  
--- OneTimeKey.java
+++ OneTimeKey.java
@@ -24,3 +24,4 @@ public class OneTimeKey {
         return encrypt(encryptedData, key);
     }
 }
+  
